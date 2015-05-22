9d2222e797f8087e18e73b7b0a65cad90cb58b4e
averted <vitiuk.alex@gmail.com>
2015-05-22 11:29:28 -0400

    .bash_profile

--- script.sh
+++ script.sh
@@ -4,6 +4,7 @@
 cp -R $HOME/.vim/* $HOME/git/configs/.vim/
 
 # configs
+cp $HOME/.bash_profile $HOME/git/configs/
 cp $HOME/.bashrc $HOME/git/configs/
 cp $HOME/.vimrc $HOME/git/configs/
 cp $HOME/.commands $HOME/git/configs/
