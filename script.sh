59a6df4889230b86d80fc86031c734bca7e06093
averted <vitiuk.alex@gmail.com>
2015-10-03 09:54:20 -0400

    script update to include .gitconfig

--- script.sh
+++ script.sh
@@ -7,3 +7,4 @@ cp -R $HOME/.vim/* $HOME/git/dotfiles/.vim/
 cp $HOME/.bash_profile $HOME/git/dotfiles/
 cp $HOME/.bashrc $HOME/git/dotfiles/
 cp $HOME/.vimrc $HOME/git/dotfiles/
+cp $HOME/.gitconfig $HOME/git/dotfiles/
