7344195c40c69c775c316b1edf57fd664fc3eb73
averted <email>
2024-09-06 14:05:51 +0200

    Update syncs

--- sync-down.sh
+++ sync-down.sh
@@ -3,13 +3,27 @@ VIM_DIR=$HOME/.vim
 DOTFILES=$HOME/git/dotfiles
 
 # vim
-cp $DOTFILES/.vim/themes/* $VIM_DIR/autoload/airline/themes
 cp $DOTFILES/.vimrc $HOME
+cp $DOTFILES/.vim/themes/* $VIM_DIR/autoload/airline/themes
+
+# vim bundle
+cp -R $DOTFILES/.vim/bundle/auto-pairs $VIM_DIR/bundle
+cp -R $DOTFILES/.vim/bundle/coc.nvim $VIM_DIR/bundle
+cp -R $DOTFILES/.vim/bundle/ctrlp.vim $VIM_DIR/bundle
+cp -R $DOTFILES/.vim/bundle/rust.vim $VIM_DIR/bundle
+cp -R $DOTFILES/.vim/bundle/vim-airline $VIM_DIR/bundle
+cp -R $DOTFILES/.vim/bundle/vim-bufferline $VIM_DIR/bundle
+cp -R $DOTFILES/.vim/bundle/vim-javascript $VIM_DIR/bundle
+cp -R $DOTFILES/.vim/bundle/vim-json $VIM_DIR/bundle
+cp -R $DOTFILES/.vim/bundle/vim-rooter $VIM_DIR/bundle
+cp -R $DOTFILES/.vim/bundle/nerdtree $VIM_DIR/bundle
 
 # dotfiles
 cp $DOTFILES/.bash_profile $HOME
 cp $DOTFILES/.bashrc $HOME
 cp $DOTFILES/.gitconfig $HOME
 
-# wezterm config
+# .config
 cp -R $DOTFILES/.config/wezterm $HOME/.config
+cp -R $DOTFILES/.config/coc $HOME/.config
+cp -R $DOTFILES/.config/zed $HOME/.config
--- sync-up.sh
+++ sync-up.sh
@@ -4,8 +4,8 @@ DOTFILES=$HOME/git/dotfiles
 # vim
 cp -R $HOME/.vim/* $DOTFILES/.vim/
 
-ls -la $HOME/.vim/bundle >> .vim.bundle
-cat $HOME/.config/coc/extensions/package.json >> .coc.extensions
+rm $DOTFILES/.vim.bundle
+ls -la $HOME/.vim/bundle >> $DOTFILES/.vim.bundle
 
 # dotfiles
 cp $HOME/.bash_profile $DOTFILES
@@ -13,4 +13,8 @@ cp $HOME/.bashrc $DOTFILES
 cp $HOME/.vimrc $DOTFILES
 cp $HOME/.gitconfig $DOTFILES
 cp $HOME/.alacritty.yml $DOTFILES
-cp -R $HOME/.config/wezterm $DOTFILES/.config/
+
+# .config
+cp -R $HOME/.config/wezterm $DOTFILES/.config
+cp -R $HOME/.config/coc $DOTFILES/.config
+cp -R $HOME/.config/zed $DOTFILES/.config
